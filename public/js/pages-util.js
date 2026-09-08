79368c316ca468a7a0f38c4c28119902706ec963
SRIHARI KATTA <frsmkgit@gmail.com>
2026-09-07 21:36:01 -0500

    Add stable slugs for service detail routes

--- public/js/pages-util.js
+++ public/js/pages-util.js
@@ -1,1 +1,12 @@
-export const help = (title, summary, detail) => ({ title, summary, detail });
+export const slugify = (value = '') => String(value)
+  .toLowerCase()
+  .normalize('NFKD')
+  .replace(/[^a-z0-9]+/g, '-')
+  .replace(/^-+|-+$/g, '');
+
+export const help = (title, summary, detail) => ({
+  title,
+  summary,
+  detail,
+  slug: slugify(title)
+});
