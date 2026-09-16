388ed59f389991cdc4f9d08bb77f8b07b0404a1b
SRIHARI KATTA <frsmkgit@gmail.com>
2026-09-16 16:25:47 -0500

    fix(phase19): restore single skip-link ownership

--- src/frontend/layouts/site-shell.js
+++ src/frontend/layouts/site-shell.js
@@ -2,5 +2,5 @@ import { footerTemplate } from '../components/footer.js';
 import { headerTemplate } from '../components/navigation.js';
 
 export function siteShell(pathName, pageContent) {
-  return `<a class="skip-link" href="#main-content">Skip to main content</a>${headerTemplate(pathName)}${pageContent}${footerTemplate()}`;
+  return `${headerTemplate(pathName)}${pageContent}${footerTemplate()}`;
 }
